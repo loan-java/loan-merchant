e66a7c172283e5717f0673926dec74bd4d87957d
shuailj <178397218@qq.com>
2019-05-29 00:05:42 +0800

    代码优化

--- src/main/java/com/mod/loan/controller/system/SystemController.java
+++ src/main/java/com/mod/loan/controller/system/SystemController.java
@@ -272,7 +272,7 @@ public class SystemController {
         String randomNum = RandomUtils.generateRandomNum(6);
         logger.info("ip={},merchant={},login_name={},登陆验证未通过,向手机={}发送短信验证码={}", ip, manager.getMerchant(), manager.getLoginName(), manager.getUserPhone(), randomNum);
         redisMapper.set(RedisConst.USER_SECURITY_CODE + manager.getUserPhone(), randomNum, 300);
-        rabbitTemplate.convertAndSend(RabbitConst.QUEUE_SMS, new SmsMessage(manager.getMerchant(), SmsTemplate.T002.getKey(), manager.getUserPhone(), randomNum + "|5分钟"));
+        rabbitTemplate.convertAndSend(RabbitConst.QUEUE_SMS, new SmsMessage(manager.getMerchant(), SmsTemplate.T001.getKey(), manager.getUserPhone(), randomNum + "|5分钟"));
         return new ResultMessage(ResponseEnum.M2000);
     }
 
