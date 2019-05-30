06b04261ca132eb6657b8b087af010b82a9f83d6
whw <weihanwen1991@gmail.com>
2019-05-30 19:32:04 +0800

    逻辑修复

--- src/main/java/com/mod/loan/mapper/OrderMapper.java
+++ src/main/java/com/mod/loan/mapper/OrderMapper.java
@@ -8,6 +8,7 @@ import org.apache.ibatis.annotations.Param;
 
 import com.mod.loan.common.mapper.MyBaseMapper;
 import com.mod.loan.model.Order;
+import org.apache.ibatis.annotations.Select;
 
 public interface OrderMapper extends MyBaseMapper<Order> {
 
@@ -63,5 +64,18 @@ public interface OrderMapper extends MyBaseMapper<Order> {
 	List<Map<String, Object>> exportReport(Map<String, Object> param);
 
 	Order selectOrderById(@Param("id") Long id);
+
+	@Select("select count(*) from tb_order where merchant = #{merchant} and status in(31,32,33,34,41,42)")
+	int sucessOrderAll(@Param(value = "merchant") String merchant);
+
+	@Select("select count(*) from tb_order where merchant = #{merchant} and status in(31,32,33,34,41,42) and source=#{source}")
+	int sucessOrder(@Param(value = "merchant") String merchant, @Param(value = "source") Integer source);
+
+	@Select("select count(*) from tb_order where merchant = #{merchant} and source=#{source}")
+	int allOrder(@Param(value = "merchant") String merchant, @Param(value = "source") Integer source);
+
+	@Select("select ifnull(sum(a.repay_money),0) from tb_order_repay a left join tb_order b on a.order_id = b.id where b.merchant = #{merchant} " +
+			"and a.repay_status in (3) and a.repay_type in (1,7) and b.payment_type=#{paymentType}")
+	double sucessOrderForPaymentType(@Param(value = "merchant") String merchant, @Param(value = "paymentType") String paymentType);
 
 }
--- src/main/java/com/mod/loan/mapper/UserMapper.java
+++ src/main/java/com/mod/loan/mapper/UserMapper.java
@@ -7,6 +7,7 @@ import org.apache.ibatis.annotations.Param;
 
 import com.mod.loan.common.mapper.MyBaseMapper;
 import com.mod.loan.model.User;
+import org.apache.ibatis.annotations.Select;
 
 public interface UserMapper extends MyBaseMapper<User> {
 
@@ -33,5 +34,11 @@ public interface UserMapper extends MyBaseMapper<User> {
 	 * @return
 	 */
 	List<Map<String, Object>> exportUserOriginReport(Map<String, Object> param);
+
+	@Select("select count(*) from tb_user where merchant =#{merchant} and user_origin=#{userOrigin} ")
+	int countAllUser(@Param(value = "merchant") String merchant, @Param(value = "userOrigin") String userOrigin);
+
+	@Select(" select count(uid) from tb_user_ident ui left join tb_user u on u.id = ui.uid where u.merchant = #{merchant}")
+	int countBindbankUserNumberAll(@Param(value = "merchant") String merchant);
 
 }
