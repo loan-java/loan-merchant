6610f0e7159ee16601f4be219123240ca0a7dbe8
shuailj <178397218@qq.com>
2019-06-30 12:47:57 +0800

    代码回滚

--- src/main/java/com/mod/loan/util/yeepay/YeePayApiRequest.java
+++ src/main/java/com/mod/loan/util/yeepay/YeePayApiRequest.java
@@ -3,7 +3,6 @@ package com.mod.loan.util.yeepay;
 import com.alibaba.fastjson.JSONObject;
 import com.mod.loan.util.MoneyUtil;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -13,15 +12,14 @@ import java.util.Map;
  * @ date 2019/6/16 12:58
  */
 @Slf4j
-@Component
 public class YeePayApiRequest {
 
     //余额查询
-    public long queryBalanceFen() {
+    public static long queryBalanceFen() {
         return MoneyUtil.yuan2Fen(queryBalance());
     }
 
-    public double queryBalance() {
+    public static double queryBalance() {
         try {
             String customerNumber = format(Config.getInstance().getValue("customerNumber"));
 
@@ -40,17 +38,4 @@ public class YeePayApiRequest {
     public static String format(String text) {
         return text == null ? "" : text.trim();
     }
-
-
-    public static void main(String[] args) throws Exception {
-        String customerNumber = format("1000046");
-
-        Map<String, Object> params = new HashMap<>();
-        params.put("customerNumber", customerNumber);
-        String uri = YeepayUtil.getUrl(YeepayUtil.customeramountQuery_URL);
-
-        JSONObject json = YeepayUtil.yeepayYOP(params, "/rest/v1.0/balance/query_customer_amount");
-        System.out.print(Double.valueOf(MoneyUtil.fen2YuanStr(MoneyUtil.yuan2Fen(json.getDoubleValue("wtjsValidAmount"))))); //wtjsValidAmount	代付代发可用余额, accountAmount	账户可用余额
-    }
-
 }
