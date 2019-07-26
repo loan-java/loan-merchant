7c2d7d37833380f895a82ec73bd6306fa6c0b8c4
shuailj <178397218@qq.com>
2019-07-26 16:06:11 +0800

    蹦蹦初始化

---
+++ src/main/java/com/mod/loan/service/CallBackBengBengService.java
@@ -0,0 +1,23 @@
+package com.mod.loan.service;
+
+import com.mod.loan.model.Order;
+
+public interface CallBackBengBengService {
+
+    /**
+     * 推送订单状态
+     *
+     */
+    void pushOrderStatus(Order order);
+
+    /*
+     * @Description:
+     * @Param: 推送还款计划
+     * @return:
+     * @Author: huijin.shuailijie
+     * @Date: 2019/5/19
+     */
+    void pushRepayPlan(Order order);
+
+
+}
