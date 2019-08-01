e555ad0a4289bcfbc6b8e9aaf007dd4356bc855e
shuailj <178397218@qq.com>
2019-08-01 09:16:53 +0800

    代码优化

--- src/main/java/com/mod/loan/service/impl/OrderServiceImpl.java
+++ src/main/java/com/mod/loan/service/impl/OrderServiceImpl.java
@@ -353,7 +353,7 @@ public class OrderServiceImpl extends BaseServiceImpl<Order, Long> implements Or
                 data.put("merchantBalance", accountRechargeMapper.getAccountRecharge() - sum);
             } else if (MerchantEnum.isHuaShiDai(merchant)) {
                 lif = successOrderMoneyRZ * 0.3 * 0.3;
-                fkf = countFlowAmountBefore78 * 5.5 + countFlowAmountAfter78 * 3.5;
+                fkf = (countFlowAmountBefore78 + 10000) * 5.5 + (countFlowAmountAfter78 + 10000) * 3.5;
                 dxf = userSmsMapper.countUserSms() * 0.1;
                 double sum = lif + fkf + dxf;
                 data.put("merchantBalance", accountRechargeMapper.getAccountRecharge() - sum);
